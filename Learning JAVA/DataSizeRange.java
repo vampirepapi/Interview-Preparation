a9e783ca556d538eb08be3acf80e879cac8dd6f5
vampirepapi <shubhamsourabh8@gmail.com>
2023-08-24 22:27:16 +0530

    changes

--- Learning JAVA/DataSizeRange.java
+++ Learning JAVA/DataSizeRange.java
@@ -5,7 +5,7 @@ class DataSizeRange{
 		System.out.println("Byte Min: "+ Byte.MIN_VALUE);
 		System.out.println("Byte Max: "+ Byte.MAX_VALUE);
 		System.out.println("Byte Size(bits): "+ Byte.SIZE);
-		System.out.println("Byte Bytes: "+ Byte.BYTES);
+		System.out.println("Byte Bytes: "+ Byte.BYTES); 
 
 		System.out.println("Int Min: "+ Integer.MIN_VALUE);
 		System.out.println("Int Max: "+ Integer.MAX_VALUE);
--- Learning JAVA/Loops/WhileLoop.java
+++ Learning JAVA/Loops/WhileLoop.java
@@ -1,2 +1,11 @@
-package Loops;public class WhileLoop {
+package Loops;
+
+public class WhileLoop {
+    public static void main(String args[]){
+        int i=1,j=100;
+        while (i<j){
+            System.out.println(i);
+            i=i*2;
+        }
+    }
 }
